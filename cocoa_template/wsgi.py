f0df9e339b2f2cb93eb062ac9bac339dd18bc028
Donovan Preston <donovanpreston@gmail.com>
2023-06-13 12:05:13 -0400

    rename everything to cocoa_template

--- cocoa_template/wsgi.py
+++ cocoa_template/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sesqui project.
+WSGI config for cocoa_template project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sesqui.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cocoa_template.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sesqui.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cocoa_template.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
